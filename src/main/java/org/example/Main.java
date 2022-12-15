3cab81620116af1b1d1d61086d69d4e482a09f20
tekcay <pierreblc@hotmail.fr>
2022-12-15 15:52:41 +0100

    added a way to add a Personne to the database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,25 @@
 package org.example;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.example.model.Personne;
+
+import javax.persistence.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("demojpa");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Personne nouvellePersonne = new Personne();
+        nouvellePersonne.setNom("toto");
+        nouvellePersonne.setPrenom("tata");
+
+        em.persist(nouvellePersonne);
+        transaction.commit();
+        em.close();
+        emf.close();
+
     }
 }
