7b9d2ba836ba5a2eff0064d1728d088cb5505984
tekcay <pierreblc@hotmail.fr>
2022-12-15 14:10:51 +0100

    removed scope of the mysql connector java in the pom.xml. Added Entity Manager, refactored the database and entity to demojpa to avoid typos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demojpa");
+        EntityManager em = emf.createEntityManager();
     }
 }
